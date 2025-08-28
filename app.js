d21c90322ae6253093fa1e592c35b282daddd057
DorianCrds <doriancardoso00@gmail.com>
2025-08-28 10:47:45 +0200

    test ci-cd

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello from GitHub Actions 2!');
+    res.send('Hello from GitHub Actions !');
 });
 
 module.exports = app;
